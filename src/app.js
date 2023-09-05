0d3ef1da29cc65ceedcd9b2bb9232a0372ac4854
Amrita0987 <141110295+Amrita0987@users.noreply.github.com>
2023-09-05 15:53:05 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,39 @@
-const express = require('express');
-const app = express()
+const http = require('http');
 
-app.get("/testNode", (_req, res) => {
-    res.status(200).send("Yes the testNode endpoint worked")
-})
+ 
 
-module.exports = app;
+// Define the hostname and port where your server will listen
+
+const hostname = '127.0.0.1'; // Use '0.0.0.0' to listen on all available network interfaces
+
+const port = 3000; // You can use any available port
+
+ 
+
+// Create a simple HTTP server
+
+const server = http.createServer((req, res) => {
+
+  // Set the response header with a 200 OK status and plain text content type
+
+  res.statusCode = 200;
+
+  res.setHeader('Content-Type', 'text/plain');
+
+ 
+
+  // Send a "Hello, World!" message as the response
+
+  res.end('Hello, World!\n');
+
+});
+
+ 
+
+// Start the server and listen on the specified hostname and port
+
+server.listen(port, hostname, () => {
+
+  console.log(`Server running at http://${hostname}:${port}/`);
+
+});
