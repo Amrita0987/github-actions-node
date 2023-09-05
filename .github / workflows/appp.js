01f59b3b3dc456fac10475c39b609b9ff5f777b5
Amrita0987 <141110295+Amrita0987@users.noreply.github.com>
2023-09-05 15:48:31 +0530

    Update appp.js

--- .github / workflows/appp.js
+++ .github / workflows/appp.js
@@ -1,1 +1,39 @@
+const http = require('http');
 
+ 
+
+// Define the hostname and port where your server will listen
+
+const hostname = '127.0.0.1'; // Use '0.0.0.0' to listen on all available network interfaces
+
+const port = 3000; // You can use any available port
+
+ 
+
+// Create a simple HTTP server
+
+const server = http.createServer((req, res) => {
+
+  // Set the response header with a 200 OK status and plain text content type
+
+  res.statusCode = 200;
+
+  res.setHeader('Content-Type', 'text/plain');
+
+ 
+
+  // Send a "Hello, World!" message as the response
+
+  res.end('Hello, World!\n');
+
+});
+
+ 
+
+// Start the server and listen on the specified hostname and port
+
+server.listen(port, hostname, () => {
+
+  console.log(`Server running at http://${hostname}:${port}/`);
+
+});
